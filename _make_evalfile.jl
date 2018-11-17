80850aa3568a3716d3d96aa46c42f6718b2d369c
Layne Penney <layne@evove.tech>
2018-11-17 12:53:03 -0600

    tidy

--- _make_evalfile.jl
+++ _make_evalfile.jl
@@ -10,7 +10,6 @@ end
 function macro_make_evalfile(name::Symbol)
     namestr = string(name)
     return quote
-        export @$(esc(name))
         macro $(esc(name))(args::Vararg{Any,N} where N)
             let name = $(esc(namestr))
                 make_evalfile("$(name)", args...)
@@ -20,7 +19,6 @@ function macro_make_evalfile(name::Symbol)
 end
 macro_make_evalfile(name::QuoteNode) = macro_make_evalfile(name.value)
 
-export @make_evalfile
 macro make_evalfile(name)
     macro_make_evalfile(name)
 end
