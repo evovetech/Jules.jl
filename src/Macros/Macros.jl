c2f4816aab1e67e50228620bfd92e009db50901e
Layne Penney <layne@evove.tech>
2018-11-20 20:58:12 -0600

    different ways to wrap enum

--- src/Macros/Macros.jl
+++ src/Macros/Macros.jl
@@ -3,9 +3,11 @@ module Macros
 using Base
 using ..Log
 
-export  @parse,
-        @enum_symbol,
-        @kvenum
+export @parse, @kvenum
+
+for i in 1:3
+    eval(Meta.parse("export @enum_wrap$i"))
+end
 
 include("util.jl")
 include("expr.jl")
--- src/Macros/enum_symbol.jl
+++ src/Macros/enum_symbol.jl
@@ -17,6 +17,17 @@ function Base.push!(expr::Expr, sym::Expr)
     return nothing
 end
 
-macro enum_symbol(T, syms...)
+macro enum_wrap1(T, syms...)
     esc(:($(Expr(:macrocall, Symbol("@enum"), __source__, T, syms...))))
+end
+
+macro enum_wrap2(T, syms...)
+    esc(:(@enum($T, $(syms...))))
+end
+
+macro enum_wrap3(T, syms...)
+    args = (T, syms...)
+    expr = :(@enum)
+    push!(expr.args, args...)
+    esc(expr)
 end
