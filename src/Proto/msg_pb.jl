16ffc4f0cf3ec2256ba2c279e47bfce8e4449054
Layne Penney <layne@evove.tech>
2018-12-31 17:53:51 -0600

    update proto

--- src/Proto/msg_pb.jl
+++ src/Proto/msg_pb.jl
@@ -2,9 +2,19 @@
 using ProtoBuf
 import ProtoBuf.meta
 
+struct __enum_Msg_Kind <: ProtoEnum
+    PAYLOAD::Int32
+    CONTROL::Int32
+    __enum_Msg_Kind() = new(0,1)
+end #struct __enum_Msg_Kind
+const Msg_Kind = __enum_Msg_Kind()
+
 mutable struct Msg <: ProtoType
-    name::AbstractString
+    to_id::UInt32
+    from_id::UInt32
+    kind::Int32
+    data::Array{UInt8,1}
     Msg(; kwargs...) = (o=new(); fillunset(o); isempty(kwargs) || ProtoBuf._protobuild(o, kwargs); o)
 end #mutable struct Msg
 
-export Msg
+export Msg_Kind, Msg
